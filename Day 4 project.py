dabe3118c8b8b998f9fa220604ac13df57cbc36b
Abdullah Shethwala <ruzruk7@gmail.com>
2023-09-10 11:13:16 -0400

    Day 4 project

--- Day 4 project.py
+++ Day 4 project.py
@@ -1,2 +1,49 @@
 # day 3 was done on day 2
 
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+import random
+def rps():
+
+    choices =[rock, paper, scissors]
+    choice_player= int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors."))
+    computer_choice = random.choice(choices)
+
+    winner = {rock:scissors, paper:rock, scissors:paper}
+    print(f'You choose:\n{choices[choice_player]}\nComputer choose:\n{computer_choice}')
+    choice_player = choices[choice_player]
+    
+    if winner[choice_player] == computer_choice:
+        print('You Won')
+    elif winner[computer_choice] == choice_player:
+        print('You lose')
+    else:
+        print('Draw')
+
+rps()
+
+
